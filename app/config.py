bdf41ac28ef879dcacf4b023e152cd44916d4d02
SitongC515 <240124734@stu.vtc.edu.hk>
2025-04-28 06:57:35 +0000

    logout

--- app/config.py
+++ app/config.py
@@ -13,6 +13,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['cywong@example.com']
+    ADMINS = ['cici@example.com']
     POSTS_PER_PAGE = 3
     LANGUAGES = ['en', 'es', 'zh']
