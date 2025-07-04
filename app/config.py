f955df68544a383cd4f38b0f99235a5ac1348791
qcheung12345 <q489183@gmail.com>
2025-07-04 02:08:53 +0000

    commit

--- app/config.py
+++ app/config.py
@@ -13,18 +13,6 @@ class Config(object):
     DB_PASSWORD = "123"
     DB_REGION = "us-central1-c"  # Note: Should be region only (e.g., "us-central1"), not zone
     
-    # Flask configuration
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER') or "mailhog"
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 1025)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['cici@example.com']
-    POSTS_PER_PAGE = 3
-    LANGUAGES = ['en', 'es', 'zh']  # Fixed typo in "correct"
-    
     @property
     def SQLALCHEMY_DATABASE_URI(self):
         # First try environment variable
